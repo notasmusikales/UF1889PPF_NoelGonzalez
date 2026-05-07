2793a62f5be2643736e1ed81c233dadd30ba7dbf
noel <notasmusikales@gmail.com>
2026-05-07 09:39:33 +0100

    Add initial __init__.py and __manifest__.py files for task management module

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Gestión de tareas",
+    "version": "1.0",
+    "summary": "Gestión básica de tareas asociadas a clientes",
+    "category": "Productivity",
+    "author": "Noel Alejandro González Febles PPF1889",
+    "depends": ["base","sale","contacts"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/tarea_view.xml",
+    ],
+    "installable": True,
+    "application": True,
+}
